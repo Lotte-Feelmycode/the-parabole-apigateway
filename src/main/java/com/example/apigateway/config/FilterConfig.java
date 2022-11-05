794c2891c1703b983c71ca3047f3e45bac109579
choihyeongjun <69442251+choihyeongjun@users.noreply.github.com>
2022-11-05 21:50:53 +0900

    fix 유레카 서버 url 변경
    feature cors 추가

--- src/main/java/com/example/apigateway/config/FilterConfig.java
+++ src/main/java/com/example/apigateway/config/FilterConfig.java
@@ -7,17 +7,22 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfig {
+    private static final String ALLOWED_HEADERS = "authorization, Content-Type, Content-Length, Authorization, credential, X-XSRF-TOKEN";
+    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
+    private static final String ALLOWED_ORIGIN = "*";
+    private static final String EXPOSE_HEADERS = "*, Authorization";
+    private static final String MAX_AGE = "7200"; //2 hours (2 * 60 * 60)
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
             .route(r -> r.path("/api/v1/event/**")
-                .filters(f -> f.addRequestHeader("first-request", "first-request-header")
-                    .addResponseHeader("first-response", "first-header"))
+                .filters(f -> f.addRequestHeader("event-request", "event-request-header")
+                    .addResponseHeader("event-response", "event-header"))
                 .uri("http://localhost:8081"))
             .route(r -> r.path("/api/v1/**")
-                .filters(f -> f.addRequestHeader("parabole-request", "parabole-request-header")
-                    .addResponseHeader("parabole-response", "parabole-header"))
+                .filters(f -> f.addRequestHeader("market-request", "market-request-header")
+                    .addResponseHeader("market-response", "market-header"))
                 .uri("http://localhost:8080"))
             .build();
     }
