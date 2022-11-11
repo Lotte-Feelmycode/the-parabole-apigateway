c435d19db2281959aa4b0c68b36f68fc8be76c71
choihyeongjun <69442251+choihyeongjun@users.noreply.github.com>
2022-11-11 16:39:22 +0900

    fix

--- src/main/java/com/example/apigateway/config/FilterConfig.java
+++ src/main/java/com/example/apigateway/config/FilterConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
     private static final String ALLOWED_HEADERS = "authorization, Content-Type, Content-Length, Authorization, credential, X-XSRF-TOKEN";
-    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
+    private static final String ALLOWED_METHODS = "*";
     private static final String ALLOWED_ORIGIN = "*";
     private static final String EXPOSE_HEADERS = "*, Authorization";
     private static final String MAX_AGE = "7200"; //2 hours (2 * 60 * 60)
